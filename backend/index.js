4b80582935a4fc572d09e65d77b953a9612b14aa
Biswajit Nayak <biswajit678@gmail.com>
2025-10-25 00:44:17 +0530

    project completed

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,20 @@ import showRoutes from './routes/showRoutes.js'
 import seatRoutes from './routes/seatRoutes.js'
 import { connectDB } from "./lib/db.js"
 import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import cors from 'cors' 
+import path from 'path'
 
 dotenv.config()
 const app=express()
 
-const PORT= process.env.PORT || 3000
+const PORT= process.env.PORT || 3000;
+const __dirname = path.resolve()
 
 app.use(express.json())
 
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://book-my-show-g3ci.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
@@ -28,6 +30,13 @@ app.use("/api/theater",theaterRoutes)
 app.use("/api/show",showRoutes)
 app.use("/api/seat", seatRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "dist")));
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "dist", "index.html"));
+    });
+}
 
 connectDB()
 app.listen(PORT,()=>{
